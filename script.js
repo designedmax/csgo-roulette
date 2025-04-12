4ae0e4ba7bf038e8c98b612e51fe776b59bfc6e6
designedmax <designedmax@yandex.ru>
2025-04-12 17:39:48 +0400

    script.js

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ window.Telegram.WebApp.onEvent('ready', function() {
   // Получаем данные о пользователе через initDataUnsafe
   const user = window.Telegram.WebApp.initDataUnsafe;
 
-  // Проверяем, есть ли имя пользователя в initDataUnsafe
-  if (user) {
-    const userName = user.first_name ? user.first_name : 'Игрок';  // Используем имя, если оно есть
+  // Проверяем, есть ли имя пользователя
+  if (user && user.first_name) {
+    const userName = user.first_name;
+    // Обновляем приветствие с именем пользователя
     document.getElementById("greeting").innerText = `Привет, ${userName}!`;
   } else {
+    // Если имя не получено, показываем стандартное приветствие
     document.getElementById("greeting").innerText = "Привет, Игрок!";
   }
 });
