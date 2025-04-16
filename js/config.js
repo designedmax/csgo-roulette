f41586bad056f70b2803692d85fd1acf466b1f02
designedmax <designedmax@yandex.ru>
2025-04-16 11:20:58 +0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -13,13 +13,53 @@ const CONFIG = {
         { name: 'UMP | Сосамба', value: 1200 }
     ],
     ACHIEVEMENTS: [
-        { id: 'new_player', name: 'Новый игрок', description: 'Зарегистрировался в игре', unlocked: false },
-        { id: 'first_win', name: 'Первая победа', description: 'Выиграл хотя бы один раз', unlocked: false },
-        { id: 'lucky', name: 'Везучий', description: 'Выиграл минимум 10 раз', unlocked: false },
-        { id: 'pro', name: 'Профи', description: 'Выиграл 50 раз минимум', unlocked: false },
-        { id: 'sheep', name: 'Баран', description: 'Проиграл 10 раз минимум', unlocked: false },
-        { id: 'loser', name: 'Лошок', description: 'Проиграл 30 раз минимум', unlocked: false },
-        { id: 'big_bet', name: 'Крупняк', description: 'Сделал ставку за 1000 р', unlocked: false },
-        { id: 'rich', name: 'Богач', description: 'На счете 5000 р или больше', unlocked: false }
+        {
+            id: 'new_player',
+            name: 'Салага',
+            description: 'Добро пожаловать',
+            unlocked: false
+        },
+        {
+            id: 'first_win',
+            name: 'Вкус победы',
+            description: 'Первая победа',
+            unlocked: false
+        },
+        {
+            id: 'lucky',
+            name: 'Умелый',
+            description: '10 побед',
+            unlocked: false
+        },
+        {
+            id: 'pro',
+            name: 'Игроман',
+            description: '50 побед',
+            unlocked: false
+        },
+        {
+            id: 'sheep',
+            name: 'Бывает',
+            description: '10 поражений',
+            unlocked: false
+        },
+        {
+            id: 'loser',
+            name: 'Невезучий',
+            description: '30 поражений',
+            unlocked: false
+        },
+        {
+            id: 'big_bet',
+            name: 'Рисковый',
+            description: 'Ставка за 1К',
+            unlocked: false
+        },
+        {
+            id: 'rich',
+            name: 'Буратино',
+            description: 'На балансе 5К',
+            unlocked: false
+        }
     ]
 }; 
